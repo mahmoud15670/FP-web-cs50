a924d3f5c200b180c4b5d4b07f20f42323b68b65
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-08-14 21:34:34 +0200

    h

--- final_project/delivery/models.py
+++ final_project/delivery/models.py
@@ -4,4 +4,5 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Techer(User):
     phone = models.CharField(max_length=11)
+    cv = models.FileField(upload_to='CV', help_text='please upload a PDF file')
     pass
