85a609c98c5e968a3f7a0f8e54545af2040ab47f
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-08-16 17:46:34 +0000

    Aug 16, 2024, 5:46 PM

--- final_project/coursation/forms.py
+++ final_project/coursation/forms.py
@@ -4,5 +4,5 @@ from .models import *
 class Teacher_Form(ModelForm):
     class Meta:
         model = Techer
-        fields  = ['username', 'password', 'email_address','phone']
+        fields  = ['username', 'password', 'email','phone']
 
