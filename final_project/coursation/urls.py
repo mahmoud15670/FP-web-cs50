724335cb024bd916f6366c1bccca762f10dac245
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-08-16 17:44:59 +0000

    Aug 16, 2024, 5:44 PM

--- final_project/coursation/urls.py
+++ final_project/coursation/urls.py
@@ -4,4 +4,5 @@ urlpatterns =[
     path('', views.index, name='index'),
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
+    path('teacher/register', views.teacher_register, name='teacher_register'),
 ]
