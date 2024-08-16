f1333fc7193876bd5b645ec85480f338ac8a16fc
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-08-16 17:28:05 +0000

    Aug 16, 2024, 5:28 PM

--- final_project/coursation/admin.py
+++ final_project/coursation/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-# admin.site.register(Techer)
-# admin.site.register(Student)
-# admin.site.register(Stage)
-# admin.site.register(Exam)
+admin.site.register(Techer)
+admin.site.register(Student)
+admin.site.register(Stage)
+admin.site.register(Exam)
