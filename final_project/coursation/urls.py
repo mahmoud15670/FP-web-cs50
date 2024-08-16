0cdea0f5b08f1db63fb83a537b615dd5745d0f8e
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-08-16 16:55:26 +0000

    Aug 16, 2024, 4:55 PM

--- final_project/coursation/urls.py
+++ final_project/coursation/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns =[
     path('', views.index, name='index'),
-    path('signin', views.signin, name='signin')
+    path('login', views.signin, name='login')
 ]
