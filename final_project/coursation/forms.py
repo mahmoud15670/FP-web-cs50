ec58426833fb5e0402d5aba312a686d630680496
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-08-16 18:15:13 +0000

    Aug 16, 2024, 6:15 PM

--- final_project/coursation/forms.py
+++ final_project/coursation/forms.py
@@ -1,10 +1,13 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
 from .models import *
 
 class Teacher_Form(ModelForm):
     class Meta:
         model = Techer
         fields  = ['username', 'password', 'email','phone']
+        widgets = {
+            'confirm':f
+        }
         labels = {
             'username':'username',
             'password':'password',
