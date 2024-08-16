93818dd9b5eed5f9ddc5df0a91561a6b1a0ed6de
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-08-16 16:07:54 +0000

    Aug 16, 2024, 4:07 PM

--- final_project/delivery/views.py
+++ final_project/delivery/views.py
@@ -12,7 +12,7 @@ def signin(request):
     username = request.POST['username']
     password = request.POST['password']
 
-    user = authenticate(request, username, password)
+    user = authenticate(request, username=username, password=password)
 
     if user:
         login(request, user)
