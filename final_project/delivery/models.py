1d9c3a80532b05dc461cb0059e7dc561aae4f2f8
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-08-16 09:15:43 +0000

    Aug 16, 2024, 9:15 AM

--- final_project/delivery/models.py
+++ final_project/delivery/models.py
@@ -41,8 +41,8 @@ class Student(User):
 
 
 class Stage(models.Model):
-    techer = models.ForeignKey(to='Techer', on_delete=models.PROTECT, related_name='techers')
-    student = models.ForeignKey(to='Student', on_delete=models.PROTECT, related_name='students')
+    techer = models.ForeignKey(to='Techer', on_delete=models.PROTECT, related_name='techers', null=True)
+    student = models.ForeignKey(to='Student', on_delete=models.PROTECT, related_name='students', null=True)
     age_start = models.PositiveSmallIntegerField(choices=age_choises)
     age_end = models.PositiveSmallIntegerField(choices=age_choises)
     name = models.CharField(max_length=20)
