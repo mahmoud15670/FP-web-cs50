191c66574f821851a68bb924921d6815cdd8c065
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-08-16 15:59:39 +0000

    Aug 16, 2024, 3:59 PM

--- final_project/delivery/views.py
+++ final_project/delivery/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
@@ -8,4 +9,5 @@ def signin(request):
     if request.method != 'POST':
         return render(request, 'signin.html')
     username = request.POST['username']
-    username = request.POST['username']
+    password = request.POST['password']
+    
