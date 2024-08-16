9c7c9e29fa800be4585b03da44b2640260aefae4
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-08-16 18:28:21 +0000

    Aug 16, 2024, 6:28 PM

--- final_project/coursation/forms.py
+++ final_project/coursation/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm, PasswordInput, CharField
 from .models import *
 
 class Teacher_Form(ModelForm):
-    confirm_password = CharField(widget=PasswordInput())
+    confirm_password = PasswordInput(attrs={'autocomplete':'off'})
     class Meta:
         model = Techer
         fields  = ['username', 'password', 'email','phone']
