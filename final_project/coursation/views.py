003513d8e4b475bcb164053944e32a6e862a3536
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-08-17 21:01:08 +0000

    Sun, Aug 18, 2024, 12:01 AM +03:00

--- final_project/coursation/views.py
+++ final_project/coursation/views.py
@@ -56,4 +56,4 @@ def teacher_detail_entry(request):
         })
     user_id = request.POST['user_id']
     teacher = get_object_or_404(Techer, pk=int(user_id))
-    
+    form = Teacher_detail_form(request.POST)
