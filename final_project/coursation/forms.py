061989b6d62dc9619d8b4283a8348d3d9d88654e
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-08-18 22:02:50 +0200

    gh

--- final_project/coursation/forms.py
+++ final_project/coursation/forms.py
@@ -1,61 +1,50 @@
 from django import forms
 from .models import *
 
-# class Teacher_Form(forms.ModelForm):
-#     confirm_password = forms.CharField(
-#         widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'placeholder': 'Confirm your password'})
-#     )
-#     class Meta:
-#         model = Techer
-#         fields = ['username', 'password', 'email', 'phone']
-#         exclude = ['activation']
+class User_Form(forms.ModelForm):
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'placeholder': 'Confirm your password'})
+    )
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'phone']
+        widgets = {
+            'username': forms.TextInput(attrs={'autocomplete': 'new-password', 'placeholder':'your user name'}),
+            'password': forms.PasswordInput(attrs={'autocomplete': 'new-password', 'placeholder': 'your password'}),
+            'email': forms.EmailInput(attrs={'placeholder':'your email'}),
+            'phone': forms.TextInput(attrs={'placeholder':'your phone'})
+        }
 
-#         widgets = {
-#             'username': forms.TextInput(attrs={'autocomplete': 'off','placeholder': 'Enter your username'}),
-#             'password': forms.PasswordInput(attrs={'autocomplete': 'new-password', 'placeholder': 'Enter your password'}),
-#             'email': forms.EmailInput(attrs={'placeholder': 'Enter your email address'}),
-#             'phone': forms.TextInput(attrs={'placeholder': 'Enter your phone number'}),
-
-#         }
-
-#     def save(self, commit=True):
-#         user = super().save(commit=False)
-#         user.set_password(self.cleaned_data['password'])
-#         if commit:
-#             user.save()
-#         return user
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
 
 
-# class Teacher_detail_form(forms.ModelForm):
-#     class Meta:
-#         model = Techer
-#         fields = ['first_name', 'last_name', 'age', 'stage', 'section', 'cv', 'demo']
-#         widgets = {
-#             'first_name': forms.TextInput(attrs={'required':True}),
-#             'last_name': forms.TextInput(attrs={'required':True}),
-#         }
+class Teacher_form(forms.ModelForm):
+    model = Techer
+    fields = [ 'cv', 'demo']
+
+class Teacher_detail_form(forms.ModelForm):
+    class Meta:
+        model = Techer
+        fields = [ 'cv', 'demo']
 
 
-# class Student_form(forms.ModelForm):
-#     confirm_password = forms.CharField(
-#         widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'placeholder': 'Confirm your password'})
-#     )
-#     class Meta:
-#         model = Student
-#         fields = ['username', 'password', 'email', 'phone']
-#         exclude = ['activation']
+class Student_form(forms.ModelForm):
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'placeholder': 'Confirm your password'})
+    )
+    class Meta:
+        model = Student
+        fields = ['progress']
 
-#         widgets = {
-#             'username': forms.TextInput(attrs={'autocomplete': 'off','placeholder': 'Enter your username'}),
-#             'password': forms.PasswordInput(attrs={'autocomplete': 'new-password', 'placeholder': 'Enter your password'}),
-#             'email': forms.EmailInput(attrs={'placeholder': 'Enter your email address'}),
-#             'phone': forms.TextInput(attrs={'placeholder': 'Enter your phone number'}),
 
-#         }
-
-#     def save(self, commit=True):
-#         user = super().save(commit=False)
-#         user.set_password(self.cleaned_data['password'])
-#         if commit:
-#             user.save()
-#         return user
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
--- final_project/coursation/views.py
+++ final_project/coursation/views.py
@@ -9,67 +9,70 @@ from .forms import *
 def index(request):
     return render(request, 'index.html')
 
-# def login_view(request):
-#     if request.method != 'POST':
-#         return render(request, 'signin.html')
-#     username = request.POST['username']
-#     password = request.POST['password']
+def login_view(request):
+    if request.method != 'POST':
+        return render(request, 'signin.html')
+    username = request.POST['username']
+    password = request.POST['password']
 
-#     user = authenticate(request, username=username, password=password)
+    user = authenticate(request, username=username, password=password)
     
-#     if user:
-#         login(request, user)
-#         return HttpResponseRedirect(reverse('index'))
-#     else:
-#         return render(request, 'signin.html', {
-#             'massage':'the user name or  password is incorrect'
-#         })
+    if user:
+        login(request, user)
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        return render(request, 'signin.html', {
+            'massage':'the user name or  password is incorrect'
+        })
 
-# def logout_view(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse('index'))
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
-# def teacher_register(request):
-#     if request.method != 'POST':
-#         return render(request, 'teacher_register.html', {
-#             'form':Teacher_Form
-#         })
-#     form = Teacher_Form(request.POST)
-#     password = request.POST['password']
-#     confirm = request.POST['confirm_password']
-#     if password != confirm:
-#         return render(request, 'teacher_register.html', {
-#             'form':form,
-#             'warning':'password didnot like the confirm'
-#         })
-#     if form.is_valid():
-#         user = form.save()
-#         login(request, user)
-#         return HttpResponseRedirect(reverse('index'))
-#     return render(request, 'teacher_register.html', {
-#         'form':form,
-#         'warning':'enter correct data'
-#     })
+def teacher_register(request):
+    if request.method != 'POST':
+        return render(request, 'teacher_register.html', {
+            'form':User_Form
+        })
+    user_form = User_Form(request.POST)
+    password = request.POST['password']
+    confirm = request.POST['confirm_password']
+    if password != confirm:
+        return render(request, 'teacher_register.html', {
+            'form':user_form,
+            'warning':'password didnot like the confirm'
+        })
+    if user_form.is_valid():
+        user = user_form.save()
+        print(user)
+        teacher = Techer.objects.create(user=user)
+        teacher.save()
+        login(request, user)
+        return HttpResponseRedirect(reverse('index'))
+    return render(request, 'teacher_register.html', {
+        'form':user_form,
+        'warning':'enter correct data'
+    })
 
-# class Student_register(generic.CreateView):
-#     model = Student
-#     form_class = Student_form
-#     template_name = 'student_register.html'
+class Student_register(generic.CreateView):
+    model = Student
+    form_class = Student_form
+    template_name = 'student_register.html'
 
-#     def form_valid(self, form):
-#         user = form.save()
-#         login(self.request, user)
-#         return HttpResponseRedirect(reverse('index'))
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return HttpResponseRedirect(reverse('index'))
 
         
-# class Teacher_detail_entry(generic.UpdateView):
-#     model = Techer
-#     form_class = Teacher_detail_form
-#     template_name = 'teacher_detsil_entry.html'
-#     success_url = reverse_lazy('index')
+class Teacher_detail_entry(generic.UpdateView):
+    model = Techer
+    form_class = Teacher_detail_form
+    template_name = 'teacher_detsil_entry.html'
+    success_url = reverse_lazy('index')
     
-#     def form_valid(self, form):
-#         teacher = form.save(commit=False)
-#         teacher.activation = True
-#         teacher.save()
-#         return super().form_valid(form)
+    def form_valid(self, form):
+        teacher = form.save(commit=False)
+        teacher.activation = True
+        teacher.save()
+        return super().form_valid(form)
