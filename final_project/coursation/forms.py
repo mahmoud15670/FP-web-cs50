2b7d2bb41e57d7288cdd696091d198c804a19882
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-08-18 11:37:46 +0000

    Sun, Aug 18, 2024, 1:37 PM +02:00

--- final_project/coursation/forms.py
+++ final_project/coursation/forms.py
@@ -18,6 +18,13 @@ class Teacher_Form(forms.ModelForm):
 
         }
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
 
 class Teacher_detail_form(forms.ModelForm):
     class Meta:
