8c009b09c7160b3937bf2ea0cbac3d283cb3d618
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-08-18 13:10:03 +0000

    Sun, Aug 18, 2024, 3:10 PM +02:00

--- final_project/coursation/models.py
+++ final_project/coursation/models.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 def cv_upload_path(techer, file_name):
-    return f'delivery/{techer.id}/CV/{file_name}'
+    return f'coursation/teachers/{techer.id}/CV/{file_name}'
 
 def demo_upload_path(techer, file_name):
-    return f'delivery/{techer.id}/Demo/{file_name}'
+    return f'coursation/teachers/{techer.id}/Demo/{file_name}'
 
 def certificate_upload_path(techer, file_name):
-    return f'delivery/{techer.id}/Certificate/{file_name}'
+    return f'coursation/students/{techer.id}/Certificate/{file_name}'
 
 def age_choises():
     return [(i, i) for i in range(7, 81)]
