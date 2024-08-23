32ef3e6614ff057678beb811dddc4d67cfd36cff
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-08-23 15:20:24 +0000

    Fri, Aug 23, 2024, 6:20 PM +03:00

--- final_project/coursation/views.py
+++ final_project/coursation/views.py
@@ -113,7 +113,8 @@ class Teacher_detail_entry(generic.UpdateView):
 
 
     def form_valid(self, form):
-        if self.request.user.id != 
+        if self.request.user.id != 'df':
+            'fgf'
         teacher = form.save(commit=False)
         teacher.user.first_name = form.cleaned_data['first_name']
         teacher.user.last_name = form.cleaned_data['last_name']
