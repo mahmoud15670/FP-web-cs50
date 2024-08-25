d888cbdf1822d4f86b01594eb90f45e84fdf44c3
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-08-25 21:55:29 +0200

    xssdf

--- final_project/coursation/models.py
+++ final_project/coursation/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
 def cv_upload_path(techer, file_name):
     return f'coursation/teachers/{techer.id}/CV/{file_name}'
 
