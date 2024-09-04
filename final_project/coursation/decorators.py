349676a4de297d8344f291b5d268aa4aa3522304
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-04 16:48:52 +0000

    Wed, Sep 4, 2024, 6:48 PM +02:00

--- final_project/coursation/decorators.py
+++ final_project/coursation/decorators.py
@@ -4,15 +4,18 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from .models import *
 
+
 def teacher_test(user):
     if user.is_teacher:
         return True
     return False
 
+
 def student_test(user):
     if user.is_student:
         return True
     return False
+
 
 def teacher_access_only():
     def decorator(view):
@@ -21,10 +24,13 @@ def teacher_access_only():
             if request.user.is_authenticated:
                 if teacher_test(request.user):
                     return view(request, *args, **kwargs)
-                return HttpResponseRedirect(reverse('index'))
-            return HttpResponseRedirect(reverse('login'))
+                return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse("login"))
+
         return _wrapped_view
+
     return decorator
+
 
 def accepted_teacher():
     def decorator(view):
@@ -33,9 +39,14 @@ def accepted_teacher():
         def _wrapped_view(request, *args, **kwargs):
             if request.user.techer.activation and request.user.techer.acceptation:
                 return view(request, *args, **kwargs)
-            return HttpResponseRedirect(reverse('teacher_detail_entry', args=(request.user.id,)))
+            return HttpResponseRedirect(
+                reverse("teacher_detail_entry", args=(request.user.id,))
+            )
+
         return _wrapped_view
+
     return decorator
+
 
 def student_access_only():
     def decorator(view):
@@ -44,31 +55,43 @@ def student_access_only():
             if request.user.is_authenticated:
                 if student_test(request.user):
                     return view(request, *args, **kwargs)
-                return HttpResponseRedirect(reverse('index'))
-            return HttpResponseRedirect(reverse('login'))
+                return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse("login"))
+
         return _wrapped_view
+
     return decorator
+
 
 def course_select():
     def decorator(view):
         @accepted_teacher()
         @wraps(view)
         def _wrapped_view(request, *args, **kwargs):
-            course = get_object_or_404(Course, pk=kwargs['course_id'])
+            course = get_object_or_404(Course, pk=kwargs["course_id"])
             if course.teacher.id != request.user.techer.id:
-                return HttpResponseRedirect(reverse("course_detail", kwargs={"pk": kwargs['course_id']}))
+                return HttpResponseRedirect(
+                    reverse("course_detail", kwargs={"pk": kwargs["course_id"]})
+                )
             return view(request, *args, **kwargs)
+
         return _wrapped_view
+
     return decorator
-    
-def  unit_select():
+
+
+def unit_select():
     def decorator(view):
         @accepted_teacher()
         @wraps(view)
         def _wrapped_view(request, *args, **kwargs):
-            unit = get_object_or_404(Unit, pk=kwargs['unit_id'])
+            unit = get_object_or_404(Unit, pk=kwargs["unit_id"])
             if unit.course.teacher.id != request.user.techer.id:
-                return HttpResponseRedirect(reverse("unit_detail", kwargs={"pk": kwargs['unit_id']}))
+                return HttpResponseRedirect(
+                    reverse("unit_detail", kwargs={"pk": kwargs["unit_id"]})
+                )
             return view(request, *args, **kwargs)
+
         return _wrapped_view
+
     return decorator
