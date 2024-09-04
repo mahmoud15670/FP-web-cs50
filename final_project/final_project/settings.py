82f2e1a2b5bd05d50dc1cc8531bebafe3d34a59c
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-04 19:29:16 +0000

    Wed, Sep 4, 2024, 9:29 PM +02:00

--- final_project/final_project/settings.py
+++ final_project/final_project/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/coursation/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'coursation')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
