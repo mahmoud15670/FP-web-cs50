eed7244078ef41c62124b8b63d94a5d50299c05c
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-04 14:23:35 +0000

    Wed, Sep 4, 2024, 4:23 PM +02:00

--- final_project/coursation/decorators.py
+++ final_project/coursation/decorators.py
@@ -11,6 +11,17 @@ def student_test(user):
         return True
     return False
 
+def teacher_access_only():
+    def decorator(view):
+        @wraps(view)
+        def _wrapped_view(request, *args, **kwargs):
+            if teacher_test(request.user):
+                return view(request, *args, **kwargs)
+            return HttpResponseForbidden('you are not teacher')
+        return _wrapped_view
+    return decorator
+
+    
 def student_access_only():
     def decorator(view):
         @wraps(view)
