245a0cf6a5c198feb3889870dc6833450a8906a0
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-04 14:11:51 +0000

    Wed, Sep 4, 2024, 4:11 PM +02:00

--- final_project/coursation/decorators.py
+++ final_project/coursation/decorators.py
@@ -1,6 +1,14 @@
 from functools import wraps
 from django.http import HttpResponseForbidden
 
+def teacher_test(user):
+    if user.is_teacher:
+        return True
+    return False
+
 def student_test(user):
-    if user.is_student
+    if user.is_student:
+        return True
+    return False
+    
 def check_student(permission):
