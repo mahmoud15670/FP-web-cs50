0e108b4473ac87dc5eb181b76caab7b736bad33b
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-06 20:51:23 +0000

    Fri, Sep 6, 2024, 11:51 PM +03:00

--- final_project/coursation/tests/test_models.py
+++ final_project/coursation/tests/test_models.py
@@ -24,4 +24,4 @@ class UserTestCase(TestCase):
         user.stage = stage
         user.save()
         self.assertEqual(user.stage.name, 'youth')
-        self.assertEqual(user.stage.age_start, 10)
+        self.assertEqual(user.stage.age_start, 9)
