af50925ec347ce1e0abbb3173e94455158bf2553
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-06 15:41:34 +0000

    Fri, Sep 6, 2024, 6:41 PM +03:00

--- final_project/coursation/tests/test_models.py
+++ final_project/coursation/tests/test_models.py
@@ -9,4 +9,5 @@ class UserTestCase(TestCase):
 
     def test_first_name_label(self):
         user = User.objects.get(pk=1)
-        first_name_label =user._meta.get_field(')
+        first_name_label =user._meta.get_field('first_name').verbose_name
+        self.assertEqual(first_name_label, 'first name')
