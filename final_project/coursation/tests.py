4b0770aa65d25bfb1ddb91378df8c37bb95b1863
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-06 11:26:31 +0000

    Fri, Sep 6, 2024, 2:26 PM +03:00

--- final_project/coursation/tests.py
+++ final_project/coursation/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self) -> None:
+        User.objects.create(name='mgh', password=123, email='mgh@mgh.com')
+
+        return super().setUp()
+    def test_user_name(self):
+        mgh = User.objects.get(name='mgh')
+        self.assertEqual(mgh.email, )
