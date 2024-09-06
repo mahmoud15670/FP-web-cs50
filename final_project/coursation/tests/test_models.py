0380e487652d2f2cdfd9b3a6f26d97c4c16f24d8
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-06 15:39:45 +0000

    Fri, Sep 6, 2024, 6:39 PM +03:00

--- final_project/coursation/tests/test_models.py
+++ final_project/coursation/tests/test_models.py
@@ -7,4 +7,6 @@ class UserTestCase(TestCase):
         User.objects.create(username='foo', password=123, email='mgh@mgh.com')
         return super().setUpTestData()
 
-    
+    def test_first_name_label(self):
+        user = User.objects.get(pk=1)
+        first_name_label =user._meta.get_field(')
