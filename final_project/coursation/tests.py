7d053a42edf6c47e1c6f57d8a67fe73e36ff969d
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-06 12:35:15 +0000

    Fri, Sep 6, 2024, 3:35 PM +03:00

--- final_project/coursation/tests.py
+++ final_project/coursation/tests.py
@@ -1,26 +1,26 @@
 from django.test import TestCase, Client
 from .models import *
 
-# class UserTestCase(TestCase):
-    # def setUp(self) -> None:
-        # mgh = User.objects.create(username='mgh', password=123, email='mgh@mgh.com', is_teacher=True)
-        # Techer.objects.create(user=mgh)
-#
-        # return super().setUp()
-    # def test_user_name(self):
-        # mgh = User.objects.get(username='mgh')
-        # teacher_mgh = Techer.objects.get(user=mgh)
-        # self.assertEqual(mgh.email, 'mgh@mgh.com')
-        # self.assertEqual(teacher_mgh.user.is_teacher, True)
-#
+class UserTestCase(TestCase):
+    def setUp(self) -> None:
+        mgh = User.objects.create(username='mgh', password=123, email='mgh@mgh.com', is_teacher=True)
+        Techer.objects.create(user=mgh)
+
+        return super().setUp()
+    def test_user_name(self):
+        mgh = User.objects.get(username='mgh')
+        teacher_mgh = Techer.objects.get(user=mgh)
+        self.assertEqual(mgh.email, 'mgh@mgh.com')
+        self.assertEqual(teacher_mgh.user.is_teacher, True)
+
 class LoginTestCase(TestCase):
     def setUp(self) -> None:
         self.client = Client()
         User.objects.create(username='foo', password=123)
-
-    def login_test(self):
+        return super().setUp()
+    def test_login(self):
         user = self.client.login(username='foo', password=123)
-        self.assertFalse(user)
+        self.assertTrue(user)
 
 
 
