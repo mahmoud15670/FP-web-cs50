066c0d052819c579b07ada7240549a1b53b6ef0e
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-06 15:45:32 +0000

    Fri, Sep 6, 2024, 6:45 PM +03:00

--- final_project/coursation/tests/test_models.py
+++ final_project/coursation/tests/test_models.py
@@ -7,7 +7,7 @@ class UserTestCase(TestCase):
         User.objects.create(username='foo', password=123, email='mgh@mgh.com')
         return super().setUpTestData()
 
-    def test_first_name_label(self):
+    def test_phone_max_length(self):
         user = User.objects.get(pk=1)
         phone_max_length =user._meta.get_field('phone').max_length
         self.assertEqual(phone_max_length, 11)
