89c843fb69597d68a5f23e852555c1fe5f046cd0
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-06 12:16:10 +0000

    Fri, Sep 6, 2024, 3:16 PM +03:00

--- final_project/coursation/tests.py
+++ final_project/coursation/tests.py
@@ -16,5 +16,5 @@ class UserTestCase(TestCase):
 class LoginTestCase(TestCase):
     c = Client()
     def login_test(self):
-        login = self.c.post("/login", {'username':'foo', 'password':123})
-        
+        user = self.c.login('username':'foo', 'password':123)
+
