eaa1b164d6717f8932abca1706151b8c77fac850
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-06 11:35:30 +0000

    Fri, Sep 6, 2024, 2:35 PM +03:00

--- final_project/coursation/tests.py
+++ final_project/coursation/tests.py
@@ -4,9 +4,11 @@ from .models import *
 class UserTestCase(TestCase):
     def setUp(self) -> None:
         mgh = User.objects.create(username='mgh', password=123, email='mgh@mgh.com', is_teacher=True)
-        Techer.objects.create(user=mgh, )
+        Techer.objects.create(user=mgh)
 
         return super().setUp()
     def test_user_name(self):
         mgh = User.objects.get(username='mgh')
+        teacher_mgh = Techer.objects.get(user=mgh)
         self.assertEqual(mgh.email, 'mgh@mgh.com')
+        self.assertEqual(teacher_mgh.user.is_teacher, True)
