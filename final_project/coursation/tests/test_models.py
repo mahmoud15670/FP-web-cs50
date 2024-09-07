52b8a81d81080dfc4558ff5cee55fda45edf91d7
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-07 21:50:32 +0000

    Sun, Sep 8, 2024, 12:50 AM +03:00

--- final_project/coursation/tests/test_models.py
+++ final_project/coursation/tests/test_models.py
@@ -33,6 +33,7 @@ class UserTestCase(TestCase):
         self.assertEqual(student.user.id, 1)
         self.assertTrue(student.user.is_student)
         self.assertFalse(student.user.is_teacher)
+        
 
 
     def test_stage(self):
