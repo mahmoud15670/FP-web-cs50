f2a9c899c0e45a621f609a71cc1d111454b3209d
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-07 21:10:50 +0000

    Sun, Sep 8, 2024, 12:10 AM +03:00

--- final_project/coursation/tests/test_models.py
+++ final_project/coursation/tests/test_models.py
@@ -23,7 +23,9 @@ class UserTestCase(TestCase):
     def test_user_create_teacher(self):
         self.user.create_tracher()
         teacher = Techer.objects.get(pk=1)
-        self.assertEqual()
+        self.assertEqual(teacher.user.id, 1)
+
+
     def test_stage(self):
         stage = Stage.objects.create(age_start=9, age_end=12, name='youth')
         self.assertIsNone(self.user.stage)
