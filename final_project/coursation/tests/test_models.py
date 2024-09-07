432028e7516f2d05c5bc32a95ba5c9e460fe49bf
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-07 22:12:03 +0000

    Sun, Sep 8, 2024, 1:12 AM +03:00

--- final_project/coursation/tests/test_models.py
+++ final_project/coursation/tests/test_models.py
@@ -37,10 +37,13 @@ class UserTestCase(TestCase):
     def test_user_set_stage(self):
         test_stage1 = Stage.objects.create(age_start=8, age_end=10, name='foo')
         test_stage2 = Stage.objects.create(age_start=7, age_end=9, name='foo')
+        test_stage3 = Stage.objects.create(age_start=10, age_end=18, name='foo')
         self.user.age = 9
         self.user.save()
         self.user.set_student_stage()
         self.assertEqual(self.user.stage, test_stage1)
+        self.assertEqual(self.user.stage, test_stage2)
+        self.assertNotEqual(self.user.stage, test_stage3)
 
 
     def test_stage(self):
