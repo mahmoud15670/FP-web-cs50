206086869556408a3274ad1cf33e2d224ceba1bd
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-07 22:18:03 +0000

    Sun, Sep 8, 2024, 1:18 AM +03:00

--- final_project/coursation/tests/test_models.py
+++ final_project/coursation/tests/test_models.py
@@ -35,8 +35,8 @@ class UserTestCase(TestCase):
         self.assertFalse(student.user.is_teacher)
 
     def test_user_set_stage(self):
-        test_stage1 = Stage.objects.create(age_start=8, age_end=10, name='foo')
-        test_stage2 = Stage.objects.create(age_start=7, age_end=9, name='fool')
+        test_stage1 = Stage.objects.create(age_start=7, age_end=10, name='foo')
+        test_stage2 = Stage.objects.create(age_start=3, age_end=6, name='fool')
         test_stage3 = Stage.objects.create(age_start=10, age_end=18, name='foo')
         self.user.age = 9
         self.user.save()
