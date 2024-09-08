a0d19f14c8a22d6fccefbf227742330dc6c25a67
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-08 13:02:57 +0000

    Sun, Sep 8, 2024, 3:02 PM +02:00

--- final_project/coursation/tests/test_forms.py
+++ final_project/coursation/tests/test_forms.py
@@ -6,3 +6,6 @@ class UserFormTestCase(TestCase):
     def test_confirm_password_field_label(self):
         form = User_Form()
         self.assertTrue(form.fields['confirm_password'].label is None or form.fields['confirm_password'].label == 'confirm password')
+
+    def test_clean_age(self):
+        form = User_Form()
