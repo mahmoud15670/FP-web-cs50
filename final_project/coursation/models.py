58d22414cadbad52cd990e9625f25f92d7505d79
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-08 12:11:28 +0000

    Sun, Sep 8, 2024, 2:11 PM +02:00

--- final_project/coursation/models.py
+++ final_project/coursation/models.py
@@ -40,7 +40,7 @@ def age_choises():
 
 class User(AbstractUser):
     phone = models.CharField(verbose_name="phone", max_length=11)
-    age = models.PositiveSmallIntegerField(null=True)
+    age = models.PositiveSmallIntegerField(choices=age_choises(),null=True)
     stage = models.ForeignKey(
         to="Stage",
         on_delete=models.PROTECT,
