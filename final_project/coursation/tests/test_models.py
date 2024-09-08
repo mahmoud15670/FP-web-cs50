b520e9d0068625cae850fd1cee8cce000e59c4d3
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-08 09:53:14 +0000

    Sun, Sep 8, 2024, 11:53 AM +02:00

--- final_project/coursation/tests/test_models.py
+++ final_project/coursation/tests/test_models.py
@@ -94,4 +94,6 @@ class StageTestCase(TestCase):
     
     def test_stage_invaild_create(self):
         stage1 = Stage.objects.create(age_start=2, age_end=5, name='foo')
+        stage2 = Stage.objects.create(age_start=10, age_end=8, name='foo')
         self.assertEqual(stage1.age()[1], "age must in 7 to 80 years")
+        self.assertEqual(stage1.age()[1], f"age must in range of {self.age_start} and {self.age_end}")
