d039f02f55563acd04b93ddb200906adf5c715c4
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-08 17:11:40 +0000

    Sun, Sep 8, 2024, 7:11 PM +02:00

--- final_project/coursation/tests/test_views.py
+++ final_project/coursation/tests/test_views.py
@@ -8,7 +8,9 @@ class UserLoginViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('signin.html', [template.name for template in response.templates])
         self.assertEqual(response.resolver_match.func, login_view)
+    
     def test_login_user(self):
         User.objects.create(username='foo', password='123')
+        response = self.client.post('/login')
         self.client.login(username='foo', password='123')
         
