11e3693f3e3ba879e8fc702b259b30ae9fdac176
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-08 19:24:16 +0000

    Sun, Sep 8, 2024, 9:24 PM +02:00

--- final_project/coursation/tests/test_views.py
+++ final_project/coursation/tests/test_views.py
@@ -2,32 +2,40 @@ from django.test import TestCase
 
 from coursation.views import *
 
+
 class UserLoginViewTestCase(TestCase):
-    def test_login_url(self):
-        response = self.client.get('/login')
-        self.assertEqual(response.status_code, 200)
-        self.assertNotIn('massage', response.context)
-        self.assertIn('signin.html', [template.name for template in response.templates])
-        self.assertEqual(response.resolver_match.func, login_view)
-    
-    def test_login_user(self):
-        user = User.objects.create(username= "foo")
-        user.set_password('123')
+
+    def setUp(self) -> None:
+        user = User.objects.create(username="foo")
+        user.set_password("123")
         user.save()
-        response = self.client.post('/login', {'username':'foo', 'password':'123'}, follow=True)
+        return super().setUp()
+
+    def test_login_url(self):
+        response = self.client.get("/login")
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn("massage", response.context)
+        self.assertEqual(response.resolver_match.func, login_view)
+        self.assertTemplateUsed(response, "signin.html")
+
+    def test_login_user(self):
+
+        response = self.client.post(
+            "/login", {"username": "foo", "password": "123"}, follow=True
+        )
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.wsgi_request.user.is_authenticated)
-        self.assertEqual(response.wsgi_request.user.username, 'foo')
-        self.assertRedirects(response, '/')
-        self.assertIn('index.html', [template.name for template in response.templates])
-    
+        self.assertEqual(response.wsgi_request.user.username, "foo")
+        self.assertRedirects(response, "/")
+        self.assertTemplateUsed(response, "index.html")
+
     def test_invalid_login(self):
-        user = User.objects.create(username= "foo")
-        user.set_password('123')
-        user.save()
-        response = self.client.post('/login', {'username':'foo', 'password':'1235'}, follow=True)
+        response = self.client.post(
+            "/login", {"username": "foo", "password": "1235"}, follow=True
+        )
         self.assertEqual(response.status_code, 200)
-        self.assertIn('massage', response.context)
-        self.assertEqual(response.context['massage'], "the user name or  password is incorrect")
-        self.assertIn('signin.html', [template.name for template in response.templates])
-        
+        self.assertIn("massage", response.context)
+        self.assertEqual(
+            response.context["massage"], "the user name or  password is incorrect"
+        )
+        self.assertTemplateUsed(response, "signin.html")
