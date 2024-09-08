03eb58a2b403ea7b4363726969d5368a0767e69e
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-08 16:56:20 +0000

    Sun, Sep 8, 2024, 6:56 PM +02:00

--- final_project/coursation/tests/test_views.py
+++ final_project/coursation/tests/test_views.py
@@ -6,3 +6,4 @@ class UserLoginViewTestCase(TestCase):
     def test_login_url(self):
         response = self.client.get('/login')
         self.assertEqual(response.status_code, 200)
+        self.assertIn('')
