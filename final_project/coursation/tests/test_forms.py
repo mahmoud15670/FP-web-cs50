00e0b716f2ac9a88b9ae28fe2683b08039f4a5ec
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-08 13:00:16 +0000

    Sun, Sep 8, 2024, 3:00 PM +02:00

--- final_project/coursation/tests/test_forms.py
+++ final_project/coursation/tests/test_forms.py
@@ -3,4 +3,6 @@ from django.test import TestCase
 from coursation.forms import *
 
 class UserFormTestCase(TestCase):
-    def confirm_password_field_lable
+    def confirm_password_field_label(self):
+        form = User_Form()
+        self.assertTrue(form.fields['confirm_password'].la)
