9df0c0a61c0bfcf63dd3636f9fcc2a982d132890
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-08 12:17:46 +0000

    Sun, Sep 8, 2024, 2:17 PM +02:00

--- final_project/coursation/forms.py
+++ final_project/coursation/forms.py
@@ -29,7 +29,7 @@ class User_Form(forms.ModelForm):
     def save(self, commit=True):
         age = self.changed_data["age"]
         user = super().save(commit=False)
-        user.v
+        user.stage.age_isvalid()
         user.set_password(self.cleaned_data["password"])
         if commit:
             user.save()
