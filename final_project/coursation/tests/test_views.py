bde024e16072f810c02a416be7a61f784e0ac98d
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-08 16:46:18 +0000

    Sun, Sep 8, 2024, 6:46 PM +02:00

--- final_project/coursation/tests/test_views.py
+++ final_project/coursation/tests/test_views.py
@@ -2,3 +2,7 @@ from django.test import TestCase
 
 from coursation.views import *
 
+class UserLoginViewTestCase(TestCase):
+    def test_login_url(self):
+        c = self.client()
+        response = c.
