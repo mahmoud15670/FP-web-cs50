8c59eeb258e86fbc133a18f6e714cb41c84b4f38
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-08 14:29:03 +0000

    Sun, Sep 8, 2024, 4:29 PM +02:00

--- final_project/coursation/tests/test_forms.py
+++ final_project/coursation/tests/test_forms.py
@@ -16,7 +16,9 @@ class UserFormTestCase(TestCase):
             'password':123, 
             'phone':'123', 
             'age': 7,
-            'stage':Stage.objects.create(name='foo')
+            'stage':Stage.objects.create(age_start=7, age_end=10, name="foo"),
+            'exam':'dfdfd',
+            'section':
         }
         form1 = User_Form(data={})
         self.assertTrue(form1.is_valid())
