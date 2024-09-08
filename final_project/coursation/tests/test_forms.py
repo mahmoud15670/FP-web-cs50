6324213ea29e90ebbd73e77567213c05c3ab7935
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-08 13:13:59 +0000

    Sun, Sep 8, 2024, 3:13 PM +02:00

--- final_project/coursation/tests/test_forms.py
+++ final_project/coursation/tests/test_forms.py
@@ -6,7 +6,9 @@ class UserFormTestCase(TestCase):
     def test_confirm_password_field_label(self):
         form = User_Form()
         self.assertTrue(form.fields['confirm_password'].label is None or form.fields['confirm_password'].label == 'confirm password')
-        self.assertEqual(form.fields['confirm_password'])
+        self.assertEqual(form.fields['age'].help_text, 'age 7 to 80 years')
+        self.assertEqual(form.fields['stage'].help_text, "choose the stage you want to learn thim")
+        self.assertEqual(form.fields['section'].help_text, "choose your subject")
 
     def test_clean_age(self):
         form = User_Form()
