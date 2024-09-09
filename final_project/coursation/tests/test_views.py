bef5ead799711c5b5cc94ebaedbc8a90215f045c
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-09 11:32:18 +0000

    Mon, Sep 9, 2024, 1:32 PM +02:00

--- final_project/coursation/tests/test_views.py
+++ final_project/coursation/tests/test_views.py
@@ -276,6 +276,6 @@ class TeacherEntryViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.wsgi_request.user.is_authenticated)
         self.assertIn('form', response.context)
-        self.assertEqual(response.context['form'].fields, Teacher_form.fields)
+        self.assertEqual(response.context['form']['fields'], Teacher_form.fields)
         self.assertTemplateUsed(response, "teacher_detsil_entry.html")
 
