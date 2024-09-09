7acf2e7f31018b651e081692f537ea36b83235b3
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-09 11:02:46 +0000

    Mon, Sep 9, 2024, 1:02 PM +02:00

--- final_project/coursation/tests/test_views.py
+++ final_project/coursation/tests/test_views.py
@@ -267,4 +267,6 @@ class TeacherEntryViewTestCase(TestCase):
         self.client.login(username="baz", password="123")
         response = self.client.get('/teacher/1/detsil/entry', follow=True)
         self.assertEqual(response.status_code, 200)
-        
+        self.assertRedirects(response, '/')
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+        self.assertTemplateUsed(response, 'index.html')
