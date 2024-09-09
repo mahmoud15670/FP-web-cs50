f75164dc7d14b9f704e43765e431932b4ad86f3b
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-09 20:18:01 +0000

    Mon, Sep 9, 2024, 10:18 PM +02:00

--- final_project/coursation/tests/test_views.py
+++ final_project/coursation/tests/test_views.py
@@ -457,9 +457,9 @@ class CourseDeleteViewTestCase(TestCase):
         teacher.acceptation = True
         teacher.activation = True
         teacher.save()
+        self.client.login(username='foo', password='123')
         response = self.client.get('/course/1/delete', follow=True)
         self.assertEqual(response.status_code, 200)
-        print(response.content)
         self.assertIn('delete_object', response.context)
         self.assertEqual(response.context['delete_object'], Course.objects.get(pk=1))
         self.assertTemplateUsed(response, 'delete.html')
