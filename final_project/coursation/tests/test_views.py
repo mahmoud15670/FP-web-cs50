281008ee2f22883562cbbd3707febf68a3023a7b
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-09 17:16:45 +0000

    Mon, Sep 9, 2024, 7:16 PM +02:00

--- final_project/coursation/tests/test_views.py
+++ final_project/coursation/tests/test_views.py
@@ -361,12 +361,14 @@ class CourseCreateViewtestCase(TestCase):
         return super().setUpTestData()
     def setUp(self) -> None:
         stage = Stage.objects.create(age_start=7, age_end=12, name="foo")
-        skill = Skills.objects.create()
+        skill = Skills.objects.create(name='foo')
         self.data = {
             'name':'foo',
             'start_date':datetime.datetime.date(datetime.datetime.now() + datetime.timedelta(days=5)),
             'stage':stage.id,
-
+            'skill':skill.id,
+            'duration':'15w',
+            'about':'sjahkld'
         }
         return super().setUp()
     def test_no_user_get(self):
