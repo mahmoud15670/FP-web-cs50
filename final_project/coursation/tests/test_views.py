a93db55066f9c23b2aca2344b99ff35345d09451
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-09 11:25:50 +0000

    Mon, Sep 9, 2024, 1:25 PM +02:00

--- final_project/coursation/tests/test_views.py
+++ final_project/coursation/tests/test_views.py
@@ -274,5 +274,8 @@ class TeacherEntryViewTestCase(TestCase):
         self.client.login(username='foo', password='123')
         response = self.client.get('/teacher/1/detsil/entry', follow=True)
         self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
         self.assertIn('form', response.context)
-        self.assertEqual(response.context['form'], )
+        self.assertEqual(response.context['form'], Teacher_form)
+        self.assertTemplateUsed(response, "teacher_detsil_entry.html")
+
