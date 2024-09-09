ca145856d4996c9881a05c1718bd8c556771089e
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-09-09 19:37:30 +0000

    Mon, Sep 9, 2024, 9:37 PM +02:00

--- final_project/coursation/tests/test_views.py
+++ final_project/coursation/tests/test_views.py
@@ -438,7 +438,7 @@ class CourseDeleteViewTestCase(TestCase):
                 datetime.datetime.now() + datetime.timedelta(days=5)
             ),
             stage=stage,
-            skill=skill.set(),
+            skill=skill.id,
             duration='15',
             about='sjahkld'
         )
