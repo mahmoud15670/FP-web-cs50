c4b019eac08151cb93628b67f00a74fc22cad80b
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-10 17:00:02 +0000

    Tue, Sep 10, 2024, 8:00 PM +03:00

--- final_project/coursation/tests/test_views.py
+++ final_project/coursation/tests/test_views.py
@@ -587,4 +587,4 @@ class StudentEnrollViewTestCase(TestCase):
         response = self.client.get(reverse('student_enroll', kwargs={'pk':1}), follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertRedirects(response, reverse('index'))
-        self.assertIn(response.wsgi_request.user, Course.objects.get(pk=1).student.all())
+        self.assertIn(response.wsgi_request.user.student, Course.objects.get(pk=1).student.all())
