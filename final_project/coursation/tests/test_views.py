2bd279b8302c9bbe0e5a2ca0a99975fc371af77f
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2024-09-10 16:29:10 +0000

    Tue, Sep 10, 2024, 7:29 PM +03:00

--- final_project/coursation/tests/test_views.py
+++ final_project/coursation/tests/test_views.py
@@ -584,3 +584,4 @@ class StudentEnrollViewTestCase(TestCase):
         self.assertRedirects(response, reverse('index'))
     def test_student_user(self):
         self.client.login(username='bar', password='123')
+        response = 
